1fe278b96a8ac368746e5e4b477f8577f255aaa9
melwinalappatt <40822672+melwinalappatt@users.noreply.github.com>
2018-07-13 11:07:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const http = require('http');
-
 const hostname= '127.0.0.1';
 const port = 3000;
-
 const server = http.createServer((req,res)=>{
   res.StstusCode = 200;
-  res.SetHeader('content-type',text/plan);
-  res.end("Hai ... Ithionnu nirtho.....");
+  res.setHeader('content-type',text/plan);
+  res.end("Server created.");
 });
 server.listen(port,hostname,()=> {
   console.log('Server started on port'+port);
